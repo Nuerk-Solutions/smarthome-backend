e2e8d0c48960ddc4d03fcd2f05c058c95de6cbee
derech1e <nurkthomas@gmail.com>
2022-01-17 12:16:24 +0100

    Added create logbook dto

--- src/logbook/dto/create-logbook.dto.ts
+++ src/logbook/dto/create-logbook.dto.ts
@@ -1,1 +1,66 @@
-export class CreateLogbookDto {}
+import {
+  IsDateString,
+  IsEnum,
+  IsNotEmpty,
+  IsNumberString,
+  IsOptional,
+} from 'class-validator';
+
+export enum VehicleTyp {
+  'Ferrari',
+  'VW',
+}
+
+export enum AdditionalInformationTyp {
+  'Keine',
+  'Getankt',
+  'Gewartet',
+}
+
+export enum Driver {
+  'Andrea',
+  'Claudia',
+  'Oliver',
+  'Thomas',
+}
+
+export class CreateLogbookDto {
+  @IsEnum(Driver, { each: true, message: 'Driver is not valid' })
+  driver: Driver;
+
+  @IsEnum(VehicleTyp, { each: true, message: 'VehicleTyp is not valid' })
+  vehicleTyp: VehicleTyp;
+
+  @IsNotEmpty()
+  @IsNumberString()
+  currentMileAge: string;
+
+  @IsNotEmpty()
+  @IsNumberString()
+  newMileAge: string;
+
+  // distance: string;
+  // distanceCost: string;
+
+  @IsNotEmpty()
+  @IsDateString()
+  date: Date;
+
+  @IsNotEmpty()
+  driveReason: string;
+
+  @IsEnum(AdditionalInformationTyp, {
+    each: true,
+    message: 'AdditionalInformationTyp is not valid',
+  })
+  additionalInformationTyp: AdditionalInformationTyp;
+
+  @IsOptional()
+  additionalInformation: string;
+
+  @IsOptional()
+  @IsNumberString()
+  additionalInformationCost: string;
+
+  // distanceSinceLastAdditionalInformation: string;
+}
