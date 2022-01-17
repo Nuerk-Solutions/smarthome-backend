5eb52bf89950f820871d3ce37169691e4bb3222e
derech1e <nurkthomas@gmail.com>
2022-01-17 17:26:09 +0100

    Stated unit test impl

--- src/logbook/logbook.controller.spec.ts
+++ src/logbook/logbook.controller.spec.ts
@@ -1,20 +1,152 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import {
+  AdditionalInformationTyp,
+  CreateLogbookDto,
+  Driver,
+  VehicleTyp,
+} from './dto/create-logbook.dto';
 import { LogbookController } from './logbook.controller';
 import { LogbookService } from './logbook.service';
 
 describe('LogbookController', () => {
   let controller: LogbookController;
+  let service: LogbookService;
+
+  const createLogbookDto: CreateLogbookDto = {
+    driver: Driver.Andrea,
+    vehicleTyp: VehicleTyp.Ferrari,
+    currentMileAge: '123',
+    newMileAge: '456',
+    date: new Date(),
+    driveReason: 'Drive Reason',
+    additionalInformationTyp: AdditionalInformationTyp.Getankt,
+    additionalInformation: '20',
+    additionalInformationCost: '40',
+  };
+
+  const mockLogbook = {
+    driver: Driver.Andrea,
+    vehicleTyp: VehicleTyp.Ferrari,
+    currentMileAge: '123',
+    newMileAge: '456',
+    date: new Date(),
+    driveReason: 'Drive Reason',
+    distance: '333',
+    distanceCost: '66,6',
+    additionalInformationTyp: AdditionalInformationTyp.Getankt,
+    additionalInformation: '20',
+    additionalInformationCost: '40',
+    distanceSinceLastAdditionalInformation: '0',
+    _id: 'a id',
+    createdAt: 'a timestemp',
+    updatedAt: 'a timestemp',
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [LogbookController],
-      providers: [LogbookService],
+      providers: [
+        {
+          provide: LogbookService,
+          useValue: {
+            createLogbook: jest.fn().mockResolvedValue([
+              {
+                driver: Driver.Andrea,
+                vehicleTyp: VehicleTyp.Ferrari,
+                currentMileAge: '123',
+                newMileAge: '456',
+                date: new Date(),
+                driveReason: 'Drive Reason #1',
+                additionalInformationTyp: AdditionalInformationTyp.Keine,
+                additionalInformation: '',
+                additionalInformationCost: '',
+              },
+              {
+                driver: Driver.Claudia,
+                vehicleTyp: VehicleTyp.VW,
+                currentMileAge: '456',
+                newMileAge: '789',
+                date: new Date(),
+                driveReason: 'Drive Reason #2',
+                additionalInformationTyp: AdditionalInformationTyp.Getankt,
+                additionalInformation: '20.4',
+                additionalInformationCost: '26,8',
+              },
+              {
+                driver: Driver.Thomas,
+                vehicleTyp: VehicleTyp.Ferrari,
+                currentMileAge: '101112',
+                newMileAge: '131415',
+                date: new Date(),
+                driveReason: 'Drive Reason #3',
+                additionalInformationTyp: AdditionalInformationTyp.Gewartet,
+                additionalInformation: 'Windschutzscheibe ersetzt.',
+                additionalInformationCost: '300',
+              },
+            ]),
+            create: jest.fn().mockResolvedValue(createLogbookDto),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<LogbookController>(LogbookController);
+    service = module.get<LogbookService>(LogbookService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('create()', () => {
+    it('should create a new Logbook', async () => {
+      const createSpy = jest
+        .spyOn(service, 'create')
+        .mockResolvedValueOnce(mockLogbook);
+
+      await controller.create(createLogbookDto);
+      expect(createSpy).toHaveBeenCalledWith(createLogbookDto);
+    });
+  });
+
+  describe('findAll()', () => {
+    it('should return all Logbook', async () => {
+      expect(controller.findAll()).resolves.toEqual([
+        {
+          driver: Driver.Andrea,
+          vehicleTyp: VehicleTyp.Ferrari,
+          currentMileAge: '123',
+          newMileAge: '456',
+          date: new Date(),
+          driveReason: 'Drive Reason #1',
+          additionalInformationTyp: AdditionalInformationTyp.Keine,
+          additionalInformation: '',
+          additionalInformationCost: '',
+        },
+        {
+          driver: Driver.Claudia,
+          vehicleTyp: VehicleTyp.VW,
+          currentMileAge: '456',
+          newMileAge: '789',
+          date: new Date(),
+          driveReason: 'Drive Reason #2',
+          additionalInformationTyp: AdditionalInformationTyp.Getankt,
+          additionalInformation: '20.4',
+          additionalInformationCost: '26,8',
+        },
+        {
+          driver: Driver.Thomas,
+          vehicleTyp: VehicleTyp.Ferrari,
+          currentMileAge: '101112',
+          newMileAge: '131415',
+          date: new Date(),
+          driveReason: 'Drive Reason #3',
+          additionalInformationTyp: AdditionalInformationTyp.Gewartet,
+          additionalInformation: 'Windschutzscheibe ersetzt.',
+          additionalInformationCost: '300',
+        },
+      ]);
+      expect(service.findAll).toHaveBeenCalled();
+    });
   });
 });
--- src/logbook/logbook.service.spec.ts
+++ src/logbook/logbook.service.spec.ts
@@ -1,18 +1,133 @@
+import { getModelToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
+import { Model } from 'mongoose';
+import {
+  AdditionalInformationTyp,
+  CreateLogbookDto,
+  Driver,
+  VehicleTyp
+} from './dto/create-logbook.dto';
 import { LogbookService } from './logbook.service';
+import { Logbook } from './schemas/logbook.schema';
+
+const createLogbookDto: CreateLogbookDto = {
+  driver: Driver.Andrea,
+  vehicleTyp: VehicleTyp.Ferrari,
+  currentMileAge: '123',
+  newMileAge: '456',
+  date: new Date(),
+  driveReason: 'Drive Reason',
+  additionalInformationTyp: AdditionalInformationTyp.Getankt,
+  additionalInformation: '20',
+  additionalInformationCost: '40',
+};
+
+const mockLogbook = {
+  driver: Driver.Andrea,
+  vehicleTyp: VehicleTyp.Ferrari,
+  currentMileAge: '123',
+  newMileAge: '456',
+  date: new Date(),
+  driveReason: 'Drive Reason',
+  distance: '333',
+  distanceCost: '66,6',
+  additionalInformationTyp: AdditionalInformationTyp.Getankt,
+  additionalInformation: '20',
+  additionalInformationCost: '40',
+  distanceSinceLastAdditionalInformation: '0',
+  _id: 'a id',
+  createdAt: 'a timestemp',
+  updatedAt: 'a timestemp',
+};
 
 describe('LogbookService', () => {
   let service: LogbookService;
+  let model: Model<Logbook>;
+
+  const logbookArray = [
+    {
+      driver: Driver.Andrea,
+      vehicleTyp: VehicleTyp.Ferrari,
+      currentMileAge: '123',
+      newMileAge: '456',
+      date: new Date(),
+      driveReason: 'Drive Reason #1',
+      additionalInformationTyp: AdditionalInformationTyp.Keine,
+      additionalInformation: '',
+      additionalInformationCost: '',
+    },
+    {
+      driver: Driver.Claudia,
+      vehicleTyp: VehicleTyp.VW,
+      currentMileAge: '456',
+      newMileAge: '789',
+      date: new Date(),
+      driveReason: 'Drive Reason #2',
+      additionalInformationTyp: AdditionalInformationTyp.Getankt,
+      additionalInformation: '20.4',
+      additionalInformationCost: '26,8',
+    },
+  ];
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [LogbookService],
+      providers: [
+        LogbookService,
+        {
+          provide: getModelToken('Logbook'),
+          useValue: {
+            new: jest.fn().mockReturnValue(mockLogbook),
+            constructor: jest.fn().mockReturnValue(mockLogbook),
+            find: jest.fn(),
+            create: jest.fn(),
+            exec: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<LogbookService>(LogbookService);
+    model = module.get<Model<Logbook>>(getModelToken('Logbook'));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('should return all logbooks', async () => {
+    jest.spyOn(model, 'find').mockResolvedValue({
+      exec: jest.fn().mockResolvedValue(logbookArray),
+    } as any);
+    const logbooks = await service.findAll();
+    expect(logbooks).toEqual(logbookArray);
+  });
+
+  it('should create a new Logbook', async () => {
+    jest.spyOn(model, 'create').mockImplementationOnce(() => 
+      Promise.resolve({
+        driver: Driver.Andrea,
+        vehicleTyp: VehicleTyp.Ferrari,
+        currentMileAge: '123',
+        newMileAge: '456',
+        date: new Date(),
+        driveReason: 'Drive Reason',
+        additionalInformationTyp: AdditionalInformationTyp.Getankt,
+        additionalInformation: '20',
+        additionalInformationCost: '40',
+      }),
+    );
+    const newLogbook = await service.create({
+      driver: Driver.Andrea,
+      vehicleTyp: VehicleTyp.Ferrari,
+      currentMileAge: '123',
+      newMileAge: '456',
+      date: new Date(),
+      driveReason: 'Drive Reason',
+      additionalInformationTyp: AdditionalInformationTyp.Getankt,
+      additionalInformation: '20',
+      additionalInformationCost: '40',
+    });
+
+    expect(newLogbook).toEqual(mockLogbook);
+  }
 });
