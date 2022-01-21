b971a56546c8df20cf05598f708103836ed6f3ad
jojopaul88@gmail.com <nurkthomas@gmail.com>
2022-01-21 12:53:12 +0100

    Added request logger

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,25 @@
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import helmet from 'helmet';
+import morgan from 'morgan';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  const logger = new Logger('Request');
   app.use(helmet());
+  app.use(
+    morgan('tiny', {
+      stream: {
+        write: (message) => logger.log(message.replace('\n', '')),
+      },
+    }),
+  );
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       enableDebugMessages: true,
-      skipMissingProperties: true,
+      skipMissingProperties: false,
     }),
   );
   await app.listen(3000);
