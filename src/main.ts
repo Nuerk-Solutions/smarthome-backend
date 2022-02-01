8d5606028cd21d1f3db5e1a611e0547f8e3fa84e
derech1e <jojopaul88@gmail.com>
2022-02-01 20:39:35 +0100

    Fixed build

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,11 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import helmet from 'helmet';
 import { AppModule } from './app.module';
-import { RequestContextMiddleware } from './core/middleware/request-context.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(helmet());
   app.enableCors();
-  app.use(RequestContextMiddleware.rawExpressMiddleware);
 
   app.useGlobalPipes(
     new ValidationPipe({
