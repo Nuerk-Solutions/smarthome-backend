9c1df3e753d62c35e61f6a01416d1f4478a24957
derech1e <jojopaul88@gmail.com>
2022-02-14 16:14:18 +0100

    Progress commit Added middleware to prisma

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,6 +1,8 @@
 import { PrismaClient } from '.prisma/client';
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { authentications as Authentication } from '@prisma/client';
 import { ConfigService } from '@nestjs/config';
+import { encodeString, generateHash } from '../utils/hash.util';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
@@ -18,6 +20,44 @@ export class PrismaService extends PrismaClient implements OnModuleInit, OnModul
 
   async onModuleInit() {
     await this.$connect();
+
+    // Middleware for hash the password
+    this.$use(async (params, next) => {
+      if (params.model == 'authentications') {
+        // Create Action
+        if (params.action == 'create') {
+          const authentication: Authentication = params.args.data;
+          if (authentication.password) {
+            authentication.password = await generateHash(authentication.password);
+          }
+          if (authentication.emailAddress) {
+            authentication.emailAddress = authentication.emailAddress.toLowerCase();
+          }
+          params.args.data = authentication;
+        }
+
+        // Update Action
+        if (params.action == 'update') {
+          const authentication: Authentication = params.args.data;
+          if (authentication.password) {
+            const password = await generateHash(authentication.password);
+            // Todo: Check if this method works
+            this.authentications.findUnique({ where: { id: authentication.id } }).then((currentAuthentication) => {
+              if (password !== currentAuthentication.password) {
+                authentication.password = password;
+              }
+            });
+
+            if (authentication.currentHashedRefreshToken) {
+              // the token is longer than 72 characters, so it needs to be encoded first with sha256
+              const currentHashedRefreshToken = encodeString(authentication.currentHashedRefreshToken);
+              authentication.currentHashedRefreshToken = await generateHash(currentHashedRefreshToken);
+            }
+          }
+        }
+        return next(params);
+      }
+    });
   }
 
   async onModuleDestroy() {
