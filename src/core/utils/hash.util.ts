1a1f8281f02eb891916bd266e1ee1a6de54e79e4
derech1e <jojopaul88@gmail.com>
2022-02-28 18:36:15 +0100

    Renamed parameters in hash functions

--- src/core/utils/hash.util.ts
+++ src/core/utils/hash.util.ts
@@ -13,22 +13,22 @@ export function encodeString(text: string): string {
 
 /**
  * @description Compare a password with a sha256 hash
- * @param password The password to compare
+ * @param data The data to compare
  * @param hash The hash to compare with
  * @returns {boolean} True if the password matches the hash
  */
-export async function validateHash(password: string, hash: string): Promise<boolean> {
-  if (!password || !hash) {
+export async function validateHash(data: string, hash: string): Promise<boolean> {
+  if (!data || !hash) {
     return Promise.resolve(false);
   }
-  return compare(password, hash);
+  return compare(data, hash);
 }
 
 /**
  * @description Generate a random string
- * @param password The password to hash
+ * @param data The password to hash
  * @returns {string} The hashed password
  */
-export async function generateHash(password: string): Promise<string> {
-  return hash(password, 10);
+export async function generateHash(data: string): Promise<string> {
+  return hash(data, 10);
 }
