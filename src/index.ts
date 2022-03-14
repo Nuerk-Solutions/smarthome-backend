c360f11f36b94326ff41b1b348f1ae9da93bf004
derech1e <jojopaul88@gmail.com>
2022-03-14 16:46:17 +0100

    Added mission global adapters

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,33 @@ import { ExpressAdapter } from '@nestjs/platform-express';
 import * as express from 'express';
 import * as functions from 'firebase-functions';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
+import * as bodyParser from 'body-parser';
+import * as compression from 'compression';
+import helmet from 'helmet';
+import { ValidationPipe } from '@nestjs/common';
 
 const server = express();
 
 export const createNestServer = async (expressInstance) => {
   const app = await NestFactory.create(AppModule, new ExpressAdapter(expressInstance));
+
+  app.use(cookieParser());
+  app.use(helmet());
+  app.use(compression());
+  app.use(bodyParser.json({ limit: '1mb' }));
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.enableCors();
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      enableDebugMessages: true,
+      skipMissingProperties: false,
+      transform: true,
+    }),
+  );
+
 
   return app.init();
 };
@@ -20,6 +42,6 @@ export const api = functions
   .region('europe-west1')
   .runWith({
     // timeoutSeconds: 0,
-    minInstances: 1,
+    // minInstances: 1,
   })
   .https.onRequest(server);
