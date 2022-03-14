cd7286c09ccd93c289ed9582cd0c7be612eac1ad
derech1e <jojopaul88@gmail.com>
2022-03-14 16:18:59 +0100

    Added default for logbook.schema.ts

--- src/logbook/core/schemas/logbook.schema.ts
+++ src/logbook/core/schemas/logbook.schema.ts
@@ -1,13 +1,18 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document } from 'mongoose';
+import { Document, Types } from 'mongoose';
 import { Driver } from '../enums/driver.enum';
 import { VehicleTyp } from '../enums/vehicle-typ.enum';
 import { AdditionalInformationTyp } from '../enums/additional-information-typ.enum';
+import { ObjectID } from 'bson';
 
 export type LogbookDocument = Logbook & Document;
 
 @Schema({ timestamps: { createdAt: true, updatedAt: true } })
 export class Logbook {
+
+  @Prop({ type: Types.ObjectId, required: true, default: () => new ObjectID() })
+  _id: Types.ObjectId;
+
   @Prop({ type: String, required: true, enum: Driver })
   driver: Driver;
 
