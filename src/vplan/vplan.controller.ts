2403bd4b1a7de59b75e155b5698a720eaf3a707c
derech1e <jojopaul88@gmail.com>
2022-04-06 18:22:31 +0200

    wip vplan

--- src/vplan/vplan.controller.ts
+++ src/vplan/vplan.controller.ts
@@ -1,13 +1,43 @@
-import { Controller, Get, Header } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
 import { VplanService } from './vplan.service';
+import axios from 'axios';
+import { Readable } from 'stream';
 
 @Controller('vplan')
 export class VplanController {
   constructor(private readonly vplanService: VplanService) {}
 
   @Get()
-  @Header('Content-Type', 'application/pdf')
-  get() {
-    return this.vplanService.get();
+  // @HttpCode(HttpStatus.OK)
+  // @Header('Content-Type', 'application/pdf')
+  // @Header('Content-Disposition', 'attachment;filename=Test.pdf')
+  async get(@Res() response) {
+    return await axios
+      .get('http://geschuetzt.bszet.de/s-lk-vw/Vertretungsplaene/vertretungsplan-bgy.pdf', {
+        headers: {
+          Authorization: 'Basic ' + Buffer.from('bsz-et-2021:it-system#20').toString('base64'),
+        },
+        responseType: 'arraybuffer',
+      })
+      .then((res) => {
+        const buffer: Buffer = res.data;
+        const stream = new Readable();
+        stream.push(buffer);
+        stream.push(null);
+        response.req.res.set({
+          'Content-Type': 'application/pdf',
+          'Content-Length': buffer.length,
+        });
+        stream.pipe(response.req.res);
+        // response.set("Content-Type",res.headers['content-type']);
+        // response.set("Content-Disposition",res.headers['content-disposition']);
+        // res.data.pipe(response);
+        // // return res.data;
+        // return new Promise((resolve, reject) => {
+        //   response.on('finish', resolve);
+        //   response.on('error', reject);
+        // });
+      })
+      .catch((err) => err);
   }
 }
--- src/vplan/vplan.service.ts
+++ src/vplan/vplan.service.ts
@@ -1,18 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Header, Injectable } from '@nestjs/common';
 import axios from 'axios';
 
 @Injectable()
 export class VplanService {
   get() {
-    return new Promise((resolve, reject) => {
-      axios
-        .get('http://geschuetzt.bszet.de/s-lk-vw/Vertretungsplaene/vertretungsplan-bgy.pdf', {
-          headers: {
-            Authorization: 'Basic ' + Buffer.from('bsz-et-2021:it-system#20').toString('base64'),
-          },
-        })
-        .then((res) => resolve(res.data))
-        .catch((err) => reject(err));
-    });
+    return axios
+      .get('http://geschuetzt.bszet.de/s-lk-vw/Vertretungsplaene/vertretungsplan-bgy.pdf', {
+        headers: {
+          Authorization: 'Basic ' + Buffer.from('bsz-et-2021:it-system#20').toString('base64'),
+        },
+        responseType: 'stream',
+      })
+      .then((res) => {
+        return new Promise((resolve, reject) => {
+          res.data.pipe();
+        });
+      })
+      .catch((err) => err);
   }
 }
