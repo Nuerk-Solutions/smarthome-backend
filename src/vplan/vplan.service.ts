f1f3b778a46603f46478c357ad9fbd4f397675a4
derech1e <jojopaul88@gmail.com>
2022-04-13 12:01:42 +0200

    fixed vplan for mobile device

--- src/vplan/vplan.service.ts
+++ src/vplan/vplan.service.ts
@@ -1,21 +1,29 @@
-import { Header, Injectable } from '@nestjs/common';
+import { Header, Injectable, Res } from '@nestjs/common';
 import axios from 'axios';
+import { Readable } from 'stream';
 
 @Injectable()
 export class VplanService {
-  get() {
-    return axios
+
+  async get(@Res() response) {
+    await axios
       .get('http://geschuetzt.bszet.de/s-lk-vw/Vertretungsplaene/vertretungsplan-bgy.pdf', {
         headers: {
           Authorization: 'Basic ' + Buffer.from('bsz-et-2021:it-system#20').toString('base64'),
         },
-        responseType: 'stream',
+        responseType: 'arraybuffer',
       })
       .then((res) => {
-        return new Promise((resolve, reject) => {
-          res.data.pipe();
+        const buffer: Buffer = res.data;
+        const stream = new Readable();
+        stream.push(buffer);
+        stream.push(null);
+        response.req.res.set({
+          'Content-Type': 'application/pdf',
+          'Content-Length': buffer.length,
         });
+        stream.pipe(response.req.res);
       })
-      .catch((err) => err);
+      .catch((err) => err)
   }
 }
