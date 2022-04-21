b7464abedc41b6fda125dc8a88aa451400b271f0
derech1e <jojopaul88@gmail.com>
2022-04-21 12:02:39 +0200

    Added more API-Keys

--- src/authentication/authentication.service.ts
+++ src/authentication/authentication.service.ts
@@ -112,7 +112,7 @@ export class AuthenticationService {
   }
 
   public validateApiKey(apiKey: string) {
-    const apiKeys: string[] = ['ca03na188ame03u1d78620de67282882a84', 'd2e621a6646a4211768cd68e26f21228a81'];
+    const apiKeys: string[] = ['ca03na188ame03u1d78620de67282882a84', 'd2e621a6646a4211768cd68e26f21228a81', 'E1BB22EF78C616558EAC71E972B4A', '1BF31DEB232411D1E3FABA4F911CA'];
 
     return apiKeys.includes(apiKey);
   }
