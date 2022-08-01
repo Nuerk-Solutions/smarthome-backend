d0ef5cf0118ad7a321884cbedb6728ea4ea7c645
Xmg-Privat-Ubuntu <nurkthomas@gmail.com>
2022-08-01 12:39:25 +0200

    Changed max-len in .eslintrc.js from error to warning

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,7 +17,7 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    'max-len': ['error', { code: 150, ignoreUrls: true }],
+    'max-len': ['warn', { code: 150, ignoreUrls: true }],
     'prettier/prettier': ['error', { printWidth: 150 }],
     'no-shadow': 'off',
     quotes: ['error', 'single', { avoidEscape: true }],
