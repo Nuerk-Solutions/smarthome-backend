677caec65a25946a61fc01bd504c07d52b5666b4
Xmg-Privat-Ubuntu <nurkthomas@gmail.com>
2022-09-12 18:05:46 +0200

    Outsourced vplan into own function

--- src/vplan/vplan.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { VplanService } from './vplan.service';
-import { VplanController } from './vplan.controller';
-
-@Module({
-  controllers: [VplanController],
-  providers: [VplanService],
-})
-export class VplanModule {}
