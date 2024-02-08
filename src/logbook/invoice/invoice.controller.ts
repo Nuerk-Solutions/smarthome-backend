a8d8b6905b334d9476e5e58e1dd56f01c00138c5
derech1e <nurkthomas@gmail.com>
2024-02-08 23:47:46 +0100

    remove unused imports

--- src/logbook/invoice/invoice.controller.ts
+++ src/logbook/invoice/invoice.controller.ts
@@ -1,16 +1,10 @@
-import { Body, Controller, Get, HttpCode, HttpStatus, Post, Query } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { InvoiceService } from './invoice.service';
-import { CreateLogbookInvoiceDto } from '../core/dto/create-logbook-invoice.dto';
-import { ParseArray } from '../core/validation/pipes/ParseEnumArray.pipe';
-import { DriverParameter } from '../core/dto/parameters/driver.parameter';
-import { Driver } from '../core/enums/driver.enum';
 import { LogbookInvoice } from '../core/schemas/logbook-invoice.schema';
-import { DateParameter } from '../core/dto/parameters/date.parameter';
-import { StatsService } from '../stats/stats.service';
 
 @Controller()
 export class InvoiceController {
-  constructor(private readonly _invoiceService: InvoiceService, private readonly _statsService: StatsService) {}
+  constructor(private readonly _invoiceService: InvoiceService) {}
 
   @HttpCode(HttpStatus.ACCEPTED)
   @Post('/create')
