ac9c2c40a32bdb7b9a1b642bd8a691ddfd6d2283
derech1e <nurkthomas@gmail.com>
2024-02-29 23:18:34 +0100

    feat: change return type of voucher routes

--- src/logbook/voucher/voucher.controller.ts
+++ src/logbook/voucher/voucher.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Get, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { VoucherService } from './voucher.service';
 import { VoucherCreateDto, VoucherDto } from '../core/dto/voucher.dto';
+import {Voucher} from "../core/schemas/vouchers.schema";
 
 @Controller()
 export class VoucherController {
@@ -8,19 +9,21 @@ export class VoucherController {
   }
   @HttpCode(HttpStatus.OK)
   @Get('/list')
-  async list() {
+  async list(): Promise<Voucher[]> {
     return await this._voucherService.list();
   }
 
   @HttpCode(HttpStatus.CREATED)
   @Post('/create')
-  async create(@Body() voucher: VoucherCreateDto) {
-    return await this._voucherService.create(voucher);
+  async create(@Body() voucher: VoucherCreateDto): Promise<boolean> {
+    const result = await this._voucherService.create(voucher);
+    return result != null && result.isExpired == false;
   }
 
   @HttpCode(HttpStatus.OK)
   @Post('/redeem')
-  async redeem(@Body() code: VoucherDto) {
-    return await this._voucherService.redeem(code);
+  async redeem(@Body() code: VoucherDto): Promise<boolean> {
+    const result = await this._voucherService.redeem(code);
+    return result.redeemed;
   }
 }
