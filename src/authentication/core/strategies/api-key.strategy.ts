c9b3b8fa9f8e3539311dc62e5796909e3e38f109
derech1e <nurkthomas@gmail.com>
2026-04-27 20:14:21 +0200

    fix api key checking

--- src/authentication/core/strategies/api-key.strategy.ts
+++ src/authentication/core/strategies/api-key.strategy.ts
@@ -5,19 +5,24 @@ import { HeaderAPIKeyStrategy } from 'passport-headerapikey';
 
 @Injectable()
 export class ApiKeyStrategy extends PassportStrategy(HeaderAPIKeyStrategy, 'api-key') {
-  constructor(private readonly _authenticationService: AuthenticationService) {
+  constructor(private readonly authenticationService: AuthenticationService) {
     super(
       {
         header: 'authorization',
         prefix: 'Api-Key ',
       },
       false,
-      async (apiKey: string, done: (error: Error | null, data?: boolean) => void) => {
-        if (this._authenticationService.validateApiKey(apiKey)) {
-          return done(null, true);
-        }
-        return done(new UnauthorizedException(), null);
-      },
-    )
+    );
+  }
+
+  validate(apiKey: string) {
+    console.log(apiKey)
+    const validate = this.authenticationService.validateApiKey(apiKey);
+    console.log(validate)
+    if (!validate) {
+      throw new UnauthorizedException();
+    }
+
+    return true;
   }
 }
